524bc456757859cc68f477d01995df8bfccd37e2
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-11-24 14:38:28 +0530

    add h2-via-nginx

---
+++ h2-via-nginx/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {H2ViaNginxApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new H2ViaNginxApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: H2ViaNginxApplication;
+  client: Client;
+}
